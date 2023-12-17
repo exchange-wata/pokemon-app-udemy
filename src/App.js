0f9ca198b9a8ec5e7dbfd1506b529e4fc4048594
exchange-wata <exchange.wata0907@gmail.com>
2023-12-17 22:27:15 +0900

    ローディングの設定

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { getAll } from './api/pokemon'
 
 function App() {
   const initialUrl = process.env.REACT_APP_INITIAL_POKEMON_ENDPOINT;
 
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
-    const fetchAllPokemon = async () => await getAll(initialUrl)
+    const fetchAllPokemon = async () => {
+      await getAll(initialUrl); setLoading(false)
+    }
     fetchAllPokemon()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   return (
-    <div className="App"></div>
+    <div className="App">
+      {loading ? <h1>ローディング中です。</h1> : <h1>データを取得しました。</h1>}
+    </div>
   );
 }
 
