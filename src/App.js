da5401e60fcb4c9bb6f56e6ab9bfa40df8c54de5
exchange-wata <exchange.wata0907@gmail.com>
2023-12-17 20:14:43 +0900

    apiで取得テスト

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
 import './App.css';
+import { getAll } from './api/pokemon'
 
 function App() {
+  const initialUrl = process.env.REACT_APP_INITIAL_POKEMON_ENDPOINT;
+
+  useEffect(() => {
+    const fetchAllPokemon = async () => await getAll(initialUrl)
+    fetchAllPokemon()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   return (
     <div className="App"></div>
   );
---
+++ src/api/pokemon.js
@@ -0,0 +1,12 @@
+export const getAll = (url) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) throw Error('レスポンスが正しくありませんでした。')
+
+        return res.json()
+      })
+      .then((data) => resolve(data))
+      .catch((e) => reject(e))
+  })
+}
