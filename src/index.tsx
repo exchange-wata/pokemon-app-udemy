714ea5d5cf02b95b0016890d2c5a3e5ed97f68bf
exchange-wata <exchange.wata0907@gmail.com>
2023-12-18 12:50:06 +0900

    不要箇所削除

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,6 @@ import App from 'App';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 
-// ReactDOM.render(
-//   <div>
-//     <h1>Hello, Welcome to React and TypeScript</h1>
-//   </div>,
-//   document.getElementById('root')
-// );
-
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container!);
 root.render(<App />);
